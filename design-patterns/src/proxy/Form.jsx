cca98637b48ff279117550745908fb7194af6979
mahsahmd <m.ahmadpoor@hamipishgaman.com>
2023-01-09 11:09:17 +0330

    proxy pattern

--- design-patterns/src/proxy/Form.jsx
+++ design-patterns/src/proxy/Form.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react'
 import { message, userProxy } from './proxy'
 
+//TODO:showing messages does not work
 const Form = () => {
     const [age, setAge] = useState(userProxy.age);
     const [username, setUsername] = useState(userProxy.username);
-    const [messages,setMessages] = useState([]);        
+    const [messages, setMessages] = useState(message);
     const onSubmit = () => {
         userProxy.username = username;
         userProxy.age = age
-        console.log(message,'================');
         setMessages(message);
     }
     return (
@@ -18,7 +18,7 @@ const Form = () => {
             <input type="number" value={age} onChange={(e) => setAge(e.target.value)} />
             <button onClick={onSubmit}>submit</button>
             {
-                messages.length > 0 && messages.map((item,index) => <p key={index}>{item.message}</p>)
+                messages.length > 0 && messages.map((item, index) => <p key={index}>{item.message}</p>)
             }
         </div>
     )
--- design-patterns/src/proxy/proxy.js
+++ design-patterns/src/proxy/proxy.js
@@ -1,25 +1,27 @@
 const user = {
-    username:'hgmj',
-    age:'42'
+    username: 'hgmj',
+    age: '42'
 }
 export let message = [];
-export const userProxy = new Proxy(user,{
-    get:(obj,prop) => {
-        if(!obj[prop]){
+export const userProxy = new Proxy(user, {
+    get: (obj, prop) => {
+        if (!obj[prop]) {
             return `not found`
         }
-        return Reflect.get(obj,prop);
+        return Reflect.get(obj, prop);
     },
-    set:(obj,prop,value) => {
-        if(prop === 'age' && value < 18){
-            message.push({key:prop,message:'age can not be less than 18'});
-          
-        }else if(prop === 'username' && value.length < 4){
-            message.push({key:prop,message:'enter valid name'});
+    set: (obj, prop, value) => {
+        if (prop === 'age' && value < 18) {
+            let doesMessageExist = message.find((item) => item.key === prop)
+            !doesMessageExist && message.push({ key: prop, message: 'age can not be less than 18' });
 
-        }else{
+        } else if (prop === 'username' && value.length < 4) {
+            let doesMessageExist = message.find((item) => item.key === prop)
+            !doesMessageExist && message.push({ key: prop, message: 'enter valid name' });
+
+        } else {
             console.log(`${prop} changed from ${obj[prop]} to ${value}`);
-            Reflect.set(obj,prop,value)
+            Reflect.set(obj, prop, value)
         }
 
         return true;
